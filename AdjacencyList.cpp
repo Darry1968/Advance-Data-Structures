2713a7403018290603cad6c1a116379a4395639f
Darshan Soni <sonidarshan200@gmail.com>
2023-02-23 09:56:41 +0530

    some new changes

--- AdjacencyList.cpp
+++ AdjacencyList.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
-struct
-    int
-    main()
+struct edge
+{
+    char name;
+    struct edge *link;
+};
+struct Node
+{
+    char data;
+    struct edge *adj, *next;
+};
+
+class Graph
+{
+    struct Node *start;
+
+public:
+    struct Node *bptr, *ptr;
+    Graph()
+    {
+        start = NULL;
+    }
+    void ScanGraph(int v, int e)
+    {
+        ptr = new Node;
+    }
+};
+int main()
 {
 
     return 0;
