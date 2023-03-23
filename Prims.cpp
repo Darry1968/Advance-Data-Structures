da547b5aa8b2556489b9dd4a3c4e61ad833f9572
Darshan Soni <sonidarshan200@gmail.com>
2023-03-24 04:59:22 +0530

    Prims code correction

--- Prims.cpp
+++ Prims.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct Node
+struct node
 {
     int pred;
     int dist;
@@ -16,7 +16,7 @@ struct edge
 class graph
 {
     int adjmat[10][10];
-    struct Node state[10];
+    struct node state[10];
     struct edge tree[10];
     int wt;
 
@@ -25,132 +25,137 @@ public:
     {
         wt = 0;
     }
-    void intiGraph(int v, int e)
+    void initgraph(int v);
+    void scangraph(int v, int e);
+    void display(int v, int e);
+    int allperm(int v);
+    void span(int v, int e);
+};
+
+int graph::allperm(int v)
+{
+    int i;
+    for (i = 0; i < v; i++)
     {
-        for (int i = 1; i <= v; i++)
+        if (state[i].stat == 0)
         {
-            for (int j = 1; j <= v; j++)
-            {
-                adjmat[i][j] = 0;
-            }
-            state[i].pred = 0;
-            state[i].dist = 999;
-            state[i].stat = 0;
+            return 0;
         }
     }
+    return 1;
+}
 
-    int allperm(int v)
+void graph::initgraph(int v)
+{
+    int i, j;
+    for (i = 0; i < v; i++)
     {
-        for (int i = 1; i <= v; i++)
+        for (int j = 0; j < v; j++)
         {
-            if (state[i].stat == 0)
-            {
-                return 0;
-            }
-            return 1;
+            adjmat[i][j] = 0;
         }
     }
+}
 
-    void span(int v, int e)
+void graph::scangraph(int v, int e)
+{
+    int i, s, d, w;
+    for (i = 0; i < e; i++)
     {
-        int current, count, min, u1, v1;
-        for (int i = 0; i < v; i++)
+    l1:
+        cout << i + 1 << endl;
+        cout << "enter source: ";
+        cin >> s;
+        cout << "enter destination: ";
+        cin >> d;
+        cout << "enter weight: ";
+        cin >> w;
+        if ((s >= 1 && s <= v) && (d >= 1 && d <= v))
         {
-            state[i].pred = 0;
-            state[i].dist = 999;
-            state[i].stat = 0;
-        }
-        state[0].pred = 0;
-        state[0].dist = 0;
-        state[0].stat = 1;
-        current = 0;
-        count = 0;
-        while (allperm(v) != 1)
-        {
-            for (int i = 0; i < v; i++)
+            if (adjmat[s - 1][d - 1] == 0 && adjmat[d - 1][s - 1] == 0)
             {
-                if (adjmat[current][i] > 0 && state[i].stat == 0)
-                {
-                    if (adjmat[current][i] < state[i].dist)
-                    {
-                        state[i].pred = current;
-                        state[i].dist = adjmat[current][i];
-                    }
-                }
-            }
-            min = 999;
-            for (int i = 0; i < v; i++)
-            {
-                if (state[i].stat == 0 && state[i].dist < min)
-                {
-                    current = i;
-                    u1 = state[current].pred;
-                    v1 = current;
-                    state[current].stat = 1;
-                    tree[count].u = u1;
-                    tree[count].v = v1;
-                    count++;
-                    wt = wt + state[i].dist;
-                }
-            }
-        }
-        cout << "Total cost : " << wt;
-    }
-
-    void Display(int v, int e)
-    {
-        int i, j;
-        for (int i = 1; i <= v; i++)
-        {
-            for (int j = 1; j <= v; j++)
-            {
-                cout << adjmat[i][j] << "\t";
-            }
-            cout << "\n";
-        }
-    }
-
-    void scangraph(int v, int e)
-    {
-        int s, i, d, cost;
-        for (int i = 1; i <= e; i++)
-        {
-        l1:
-            cout << "Enter source : ";
-            cin >> s;
-            cout << "Enter destination : ";
-            cin >> d;
-            cout << "Enter weight : ";
-            cin >> cost;
-
-            if ((s >= 1 && s <= v) && (d >= 1 && d <= v))
-            {
-                if (adjmat[s][d] == 0 && adjmat[d][s] == 0)
-                {
-                    adjmat[s][d] = cost;
-                    adjmat[d][s] = cost;
-                }
-                else
-                {
-                    cout << "Edge is already exists" << endl;
-                    goto l1;
-                }
+                adjmat[s - 1][d - 1] = w;
+                adjmat[d - 1][s - 1] = w;
             }
             else
             {
-                cout << "Enter correct vertex !!" << endl;
+                cout << "edge already exist";
                 goto l1;
             }
         }
+        else
+        {
+            cout << "enter correct values:";
+            goto l1;
+        }
     }
-};
+}
+
+void graph::display(int v, int e)
+{
+    int i, j;
+    for (i = 0; i < v; i++)
+    {
+        cout << endl;
+        for (j = 0; j < v; j++)
+        {
+            cout << adjmat[i][j] << " ";
+        }
+    }
+}
+
+void graph::span(int v, int e)
+{
+    int current, count, min, u1, v1;
+    for (int i = 0; i < v; i++)
+    {
+        state[i].pred = 0;
+        state[i].dist = 999;
+        state[i].stat = 0;
+    }
+    state[0].pred = 0;
+    state[0].dist = 0;
+    state[0].stat = 1;
+    current = 0;
+    count = 0;
+    while (allperm(v) != 1)
+    {
+        for (int i = 0; i < v; i++)
+        {
+            if (adjmat[current][i] > 0 && state[i].stat == 0)
+            {
+                if (adjmat[current][i] < state[i].dist)
+                {
+                    state[i].pred = current;
+                    state[i].dist = adjmat[current][i];
+                }
+            }
+        }
+        min = 999;
+        for (int i = 0; i < v; i++)
+        {
+            if (state[i].stat == 0 && state[i].dist < min)
+            {
+                current = i;
+                state[current].stat = 1;
+                u1 = state[current].pred;
+                v1 = current;
+                tree[count].u = u1;
+                tree[count].v = v1;
+                count++;
+                wt = wt + state[i].dist;
+            }
+        }
+    }
+    cout << "Total cost : " << wt;
+}
+
 int main()
 {
-    graph g1;
-    g1.intiGraph(3, 3);
-    g1.scangraph(3, 3);
-    g1.Display(3, 3);
-    g1.span(3, 3);
-
-    return 0;
+    graph g;
+    g.allperm(3);
+    g.initgraph(3);
+    g.scangraph(3, 3);
+    g.display(3, 3);
+    g.span(3, 3);
 }
